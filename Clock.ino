83a549e5491df4c81dedaacea2fba21a01968645
pfory <hanka12>
2018-12-06 21:46:18 +0100

    init

---
+++ Clock.ino
@@ -0,0 +1,266 @@
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <timer.h>
+auto timer = timer_create_default(); // create a timer with default settings
+Timer<> default_timer; // save as above
+
+#include "TM1637.h"
+
+int8_t TimeDisp[] = {0x00,0x00,0x00,0x00};
+unsigned char ClockPoint = 1;
+unsigned char upd;
+unsigned char halfsecond = 0;
+
+#define CLK D2//pins definitions for TM1637 and can be changed to other ports
+#define DIO D3
+TM1637 tm1637(CLK,DIO);
+
+#define verbose
+#ifdef verbose
+  #define DEBUG_PRINT(x)         Serial.print (x)
+  #define DEBUG_PRINTDEC(x)      Serial.print (x, DEC)
+  #define DEBUG_PRINTLN(x)       Serial.println (x)
+  #define DEBUG_PRINTF(x, y)     Serial.printf (x, y)
+  #define PORTSPEED 115200
+  #define SERIAL_BEGIN           Serial.begin(PORTSPEED) 
+#else
+  #define DEBUG_PRINT(x)
+  #define DEBUG_PRINTDEC(x)
+  #define DEBUG_PRINTLN(x)
+  #define DEBUG_PRINTF(x, y)
+#endif 
+
+#include <TimeLib.h>
+#include <Timezone.h>
+WiFiUDP EthernetUdp;
+//const int timeZone = 2;     // Central European Time
+static const char     ntpServerName[]       = "tik.cesnet.cz";
+//Central European Time (Frankfurt, Paris)
+TimeChangeRule        CEST                  = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
+TimeChangeRule        CET                   = {"CET", Last, Sun, Oct, 3, 60};       //Central European Standard Time
+Timezone CE(CEST, CET);
+unsigned int          localPort             = 8888;  // local port to listen for UDP packets
+time_t getNtpTime();
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  DEBUG_PRINTLN("Entered config mode");
+  DEBUG_PRINTLN(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  DEBUG_PRINTLN(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+}
+
+void setup()
+{
+  SERIAL_BEGIN;
+  DEBUG_PRINTLN("CLOCK");
+  tm1637.set();
+  tm1637.init();
+
+  setupWifi();
+
+  DEBUG_PRINTLN("Setup TIME");
+  EthernetUdp.begin(localPort);
+  DEBUG_PRINT("Local port: ");
+  DEBUG_PRINTLN(EthernetUdp.localPort());
+  DEBUG_PRINTLN("waiting for sync");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
+  
+  printSystemTime();
+
+  timer.every(1000, TimingISR);
+}
+
+
+void loop()
+{
+  timer.tick(); // tick the timer
+}
+
+bool TimingISR(void *) {
+  DEBUG_PRINTLN();
+  printSystemTime();
+  ClockPoint = (~ClockPoint) & 0x01;
+  TimeUpdate();
+  tm1637.display(TimeDisp);
+}
+
+void TimeUpdate(void) {
+  if(ClockPoint)tm1637.point(POINT_ON);
+  else tm1637.point(POINT_OFF);
+  TimeDisp[0] = hour() / 10;
+  TimeDisp[1] = hour() % 10;
+  TimeDisp[2] = minute() / 10;
+  TimeDisp[3] = minute() % 10;
+}
+
+
+void setupWifi() {
+  DEBUG_PRINTLN("-------WIFI Setup---------");
+  WiFi.printDiag(Serial);
+    
+  // bool validConf = readConfig();
+  // if (!validConf) {
+    // DEBUG_PRINTLN(F("ERROR config corrupted"));
+  // }
+  
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+  
+  //wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
+  // IPAddress _ip,_gw,_sn;
+  // _ip.fromString(static_ip);
+  // _gw.fromString(static_gw);
+  // _sn.fromString(static_sn);
+
+  // wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
+  
+  // DEBUG_PRINTLN(_ip);
+  // DEBUG_PRINTLN(_gw);
+  // DEBUG_PRINTLN(_sn);
+
+  //if (WiFi.SSID()!="") wifiManager.setConfigPortalTimeout(60); //If no access point name has been previously entered disable timeout.
+
+  
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+  
+    //DEBUG_PRINTLN("Double reset detected. Config mode.");
+
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+  
+  wifiManager.setTimeout(30);
+  wifiManager.setConnectTimeout(10); 
+  wifiManager.setConfigPortalTimeout(60);
+  //wifiManager.setBreakAfterConfig(true);
+  
+  //set config save notify callback
+  //wifiManager.setSaveConfigCallback(saveConfigCallback);
+  
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("Clock", "password")) { 
+    DEBUG_PRINTLN("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  } 
+  
+  
+  // if (shouldSaveConfig) {
+    // saveConfig();
+  // }
+  
+  //if you get here you have connected to the WiFi
+  DEBUG_PRINTLN("CONNECTED");
+  DEBUG_PRINT("Local ip : ");
+  DEBUG_PRINTLN(WiFi.localIP());
+  DEBUG_PRINTLN(WiFi.subnetMask());
+}
+
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime() {
+  //IPAddress ntpServerIP; // NTP server's ip address
+  IPAddress ntpServerIP = IPAddress(195, 113, 144, 201);
+
+  while (EthernetUdp.parsePacket() > 0) ; // discard any previously received packets
+  DEBUG_PRINTLN("Transmit NTP Request");
+  // get a random server from the pool
+  //WiFi.hostByName(ntpServerName, ntpServerIP);
+  DEBUG_PRINT(ntpServerName);
+  DEBUG_PRINT(": ");
+  DEBUG_PRINTLN(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = EthernetUdp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      DEBUG_PRINTLN("Receive NTP Response");
+      EthernetUdp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+      // combine the four bytes (two words) into a long integer
+      // this is NTP time (seconds since Jan 1 1900):
+      unsigned long secsSince1900 = highWord << 16 | lowWord;
+      DEBUG_PRINT("Seconds since Jan 1 1900 = " );
+      DEBUG_PRINTLN(secsSince1900);
+
+      // now convert NTP time into everyday time:
+      DEBUG_PRINT("Unix time = ");
+      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+      const unsigned long seventyYears = 2208988800UL;
+      // subtract seventy years:
+      unsigned long epoch = secsSince1900 - seventyYears;
+      // print Unix time:
+      DEBUG_PRINTLN(epoch);
+	  
+      TimeChangeRule *tcr;
+      time_t utc;
+      utc = epoch;
+      
+      return CE.toLocal(utc, &tcr);
+      //return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  DEBUG_PRINTLN("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  EthernetUdp.beginPacket(address, 123); //NTP requests are to port 123
+  EthernetUdp.write(packetBuffer, NTP_PACKET_SIZE);
+  EthernetUdp.endPacket();
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding
+  // colon and leading 0
+  DEBUG_PRINT(":");
+  if(digits < 10)
+    DEBUG_PRINT('0');
+  DEBUG_PRINT(digits);
+}
+
+
+void printSystemTime(){
+  DEBUG_PRINT(day());
+  DEBUG_PRINT(".");
+  DEBUG_PRINT(month());
+  DEBUG_PRINT(".");
+  DEBUG_PRINT(year());
+  DEBUG_PRINT(" ");
+  DEBUG_PRINT(hour());
+  printDigits(minute());
+  printDigits(second());
+}
